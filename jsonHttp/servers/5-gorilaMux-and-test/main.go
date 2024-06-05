9c0545918595c51e578b88b1f97bf2af5c1dddcf
a.solomchenko <a.solomchenko@brusnika.ru>
2024-06-05 20:42:28 +0300

    mock test

--- jsonHttp/servers/5-gorilaMux-and-test/main.go
+++ jsonHttp/servers/5-gorilaMux-and-test/main.go
@@ -1,10 +1,76 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"errors"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
+
+var ErrProductNotFound = errors.New("product not found")
+
+type httpHandler struct {
+	router          *mux.Router
+	productProvider ProductProvider
+}
+
+type Product struct {
+	SKU  int64  `json:"sku"`
+	Name string `json:"name"`
+}
+
+// ProductProvider отдает товар по sku
+type ProductProvider interface {
+	GetProduct(ctx context.Context, sku int64) (*Product, error)
+}
+
+func productHandler(productProvider ProductProvider) *httpHandler {
+	router := mux.NewRouter()
+	handler := &httpHandler{
+		router:          router,
+		productProvider: productProvider,
+	}
+	router.HandleFunc("/product/{sku:[0-9]+}", handler.getProduct).Methods(http.MethodPost)
+	return handler
+}
+
+func (h *httpHandler) ServerHTTP(w http.ResponseWriter, req *http.Request) {
+	h.router.ServeHTTP(w, req)
+}
+
+func (h *httpHandler) getProduct(w http.ResponseWriter, req *http.Request) {
+	// получаем SKU товара из запроса
+	vars := mux.Vars(req)
+	sku, _ := strconv.ParseInt(vars["sku"], 10, 64)
+	if sku == 0 {
+		println("### ZERO FOUND ###")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	product, err := h.productProvider.GetProduct(req.Context(), sku)
+	if errors.Is(err, ErrProductNotFound) { // так правильнее ропверять ошибки
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		_, _ = w.Write([]byte(err.Error()))
+		return
+	}
+
+	result, err := json.Marshal(product)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		_, _ = w.Write([]byte(err.Error()))
+		return
+	}
+
+	_, _ = w.Write(result)
+}
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	_, _ = w.Write([]byte("hello world"))
@@ -13,5 +79,6 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 func newRouter() *mux.Router {
 	router := mux.NewRouter()
 	router.HandleFunc("/hello", helloHandler).Methods(http.MethodGet)
+	//router.HandleFunc("/get-product", productHandler).Methods(http.MethodPost)
 	return router
 }
--- jsonHttp/servers/5-gorilaMux-and-test/main_test.go
+++ jsonHttp/servers/5-gorilaMux-and-test/main_test.go
@@ -1,10 +1,73 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
+
+type productProviderMock struct {
+	validSKU int64
+}
+
+func (m *productProviderMock) GetProduct(_ context.Context, sku int64) (*Product, error) {
+	if sku == m.validSKU {
+		return &Product{SKU: sku, Name: "Valid product"}, nil
+	}
+	return nil, ErrProductNotFound
+}
+
+func Test_httpHandler_getProduct(t *testing.T) {
+	t.Run("invalid sku", func(t *testing.T) {
+		// arrange
+		productProviderM := &productProviderMock{}
+		mux := productHandler(productProviderM)
+
+		rec := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodPost, "/product/0", nil)
+
+		mux.ServerHTTP(rec, req)
+
+		if rec.Code != http.StatusBadRequest {
+			t.Errorf("got %d, want %d", rec.Code, http.StatusBadRequest)
+		}
+	})
+
+	t.Run("not found", func(t *testing.T) {
+		productProviderM := &productProviderMock{}
+		mux := productHandler(productProviderM)
+
+		rec := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodGet, "/product/0", nil)
+
+		mux.ServerHTTP(rec, req)
+
+		if rec.Code != http.StatusNotFound {
+			t.Errorf("got %d, want %d", rec.Code, http.StatusNotFound)
+		}
+	})
+
+	t.Run("OK", func(t *testing.T) {
+		productProviderM := &productProviderMock{validSKU: 1}
+		mux := productHandler(productProviderM)
+
+		rec := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodPost, "/product/1", nil)
+
+		mux.ServerHTTP(rec, req)
+
+		println(rec.Body.String())
+
+		if rec.Code != http.StatusOK {
+			t.Errorf("got %d, want %d", rec.Code, http.StatusOK)
+		}
+		if !strings.Contains(rec.Body.String(), `"sku":1`) {
+			t.Errorf("unexpected body in response: %q", rec.Body.String())
+		}
+	})
+}
 
 func Test_helloHandler(t *testing.T) {
 	t.Run("wrong method", func(t *testing.T) {
