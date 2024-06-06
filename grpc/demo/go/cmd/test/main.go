bcb78c5ec1d8a97a6e9fa907971b45675bddb458
a.solomchenko <a.solomchenko@brusnika.ru>
2024-06-06 22:31:49 +0300

    protobuff parser 02

---
+++ grpc/demo/go/cmd/test/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	"io"
+	"log"
+	"main/pb/my"
+	"os"
+)
+
+func main() {
+	file, err := os.OpenFile("../output.bin", os.O_RDONLY, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	params := &my.SellerParams{}
+	err = proto.Unmarshal(result, params)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("result:\n%+v\n\n\n", params)
+	fmt.Printf("first item:\n%+v\n\n\n", params.GetResult()[0])
+	fmt.Println()
+	fmt.Println()
+	fmt.Printf("first item:\n%+v\n\n\n", params.GetResult()[0].GetParams()["not_exist"]) // найдет nil (нулевой значение)
+	fmt.Println()
+	fmt.Println()
+	fmt.Printf("first item:\n%+v\n\n\n", params.GetResult()[0].GetParams()["not_exist"].GetDouble()) // найдет 0
+}
